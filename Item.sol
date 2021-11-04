e510c7f09ffb338878728eac930db19c77afb9a4
Kasshern <1400536468@qq.com>
2021-11-03 20:59:38 -0500

    Fixed inconsistencies in Item and ItemManager contracts

--- Item.sol
+++ Item.sol
@@ -3,23 +3,23 @@ pragma solidity ^0.8.0;
 import "./ItemManager.sol";
 
 contract Item {
-    uint priceInWei;
-    uint paidWei;
-    uint index;
+    uint public priceInWei;
+    uint public paidWei;
+    uint public index;
 
     ItemManager parentContract;
 
-    constructor(ItemManager _parentContract, uint _priceInWei, uint _index) {
+    constructor(ItemManager _parentContract, uint _priceInWei, uint _index) public {
         priceInWei = _priceInWei;
         index = _index;
-        parentContract = _[parentContract];
+        parentContract = _parentContract;
     }
 
     receive() external payable {
         require(msg.value == priceInWei, "Partial payments are not accepted at this time");
         require(paidWei == 0, "Item has already been pair for");
         paidWei += msg.value;
-        (bool success) = address(parentConract).call{value: msg.value} ( abi.encodeWithSignature("triggerPayment(uint256)", index));
+        (bool success, ) = address(parentContract).call{value: msg.value} ( abi.encodeWithSignature("triggerPayment(uint256)", index));
         require(success, "Item was not delivered");
     }
 
--- ItemManager.sol
+++ ItemManager.sol
@@ -1,39 +1,45 @@
 pragma solidity ^0.8.0;
 
-contract ItemManager {
+import "./Item.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ItemManager is Ownable {
 
     enum SupplyChainSteps{Created, Paid, Delivered}
 
     struct S_Item {
+        Item _item;
         ItemManager.SupplyChainSteps _step;
         string _identifier;
-        uint _priceInWei;
     }
 
     mapping(uint => S_Item) public items;
     uint index;
 
-    event SupplyChainStep(uint _itemIndex, uint _step);
+    event SupplyChainStep(uint _itemIndex, uint _step, address _address);
 
-    function createItem(string memory _identifier, uint _priceInWei) public {
-        items[index]._priceInWei = _priceInWei ;
+    function createItem(string memory _identifier, uint _priceInWei) public onlyOwner {
+        Item item = new Item(this, _priceInWei, index);
+        items[index]._item = item;
         items[index]._step = SupplyChainSteps.Created;
         items[index]._identifier = _identifier;
-        emit SupplyChainStep(index, uint(items[index]._step));
+        emit SupplyChainStep(index, uint(items[index]._step), address(item));
         index++;
     }
 
     function triggerPayment(uint _index) public payable {
-        require(items[_index]._priceInWei <= msg.value, "Not fully paid");
-        require(items[_index]._step == SupplyChainSteps.Created, "Item is beyond the Create stage in the supply chain");
+        Item item = items[_index]._item;
+        require(address(item) == msg.sender, "Items can only SelfUpdate");
+        require(item.priceInWei() == msg.value, "Item not fully paid yet");
+        require(items[_index]._step == SupplyChainSteps.Created, "Item is already beyond the Create state");
         items[_index]._step = SupplyChainSteps.Paid;
-        emit SupplyChainStep(_index, uint(items[index]._step));
+        emit SupplyChainStep(_index, uint(items[index]._step), address(item));
     }
 
-    fuction triggerDelivery(uint _index) public {
+    function triggerDelivery(uint _index) public onlyOwner {
         require(items[_index]._step == SupplyChainSteps.Paid, "Item is not in the paid state, and cannot be delievered");
         items[_index]._step = SupplyChainSteps.Delivered;
-        emit SupplyChainStep(_index, uint(items[_index]._step));
+        emit SupplyChainStep(_index, uint(items[_index]._step), address(items[_index]._item));
     }
 
 }
